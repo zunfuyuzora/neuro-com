5aa2bdf07e59293c7eda47ace79341effa0da4b9
zunkazaragi <malikal.j4@gmail.com>
2020-06-29 09:48:44 +0700

    Printable Data

--- resources/views/admin/group/index.blade.php
+++ resources/views/admin/group/index.blade.php
@@ -7,9 +7,11 @@
         <thead>
             <tr>
                 <th></th>
-                <th>Name</th>
+                <th>Group Name</th>
+                <th>ID</th>
                 <th>Creator</th>
                 <th>Created At</th>
+                <th>Total Member</th>
                 <th></th>
             </tr>
         </thead>
@@ -19,8 +21,10 @@
             <tr>
                 <td>{{$i++}}</td>
                 <td>{{$g->name}}</td>
+                <td>{{$g->id}}</td>
                 <td>{{$g->getMembers->where('access','creator')->first()->user->full_name}}</td>
                 <td>{{$g->created_at}}</td>
+                <td>{{$g->getMembers->count()}}</td>
                 <td class="text-center"><a href="{{route('admin.group.detail', $g->id)}}" class="btn btn-sm btn-primary">View</a></td>
             </tr>
             @endforeach
@@ -31,13 +35,23 @@
 @endsection
 
 @push('css')
-    <link rel="stylesheet" href="{{asset('/vendor/datatable/datatable.css')}}">
+    <link rel="stylesheet" href="{{asset('/vendor/datatable/datatables.min.css')}}">
 @endpush
 @push('script')
-<script src="{{asset('vendor/datatable/datatable.js')}}"></script>
+<script src="{{asset('vendor/datatable/datatables.min.js')}}"></script>
 <script>
     $(document).ready(function(){
-        $('#group_table').DataTable();
+        $('#group_table').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        columns: [0,1,2,3,4,5]
+                    }
+                }
+            ]
+        })
     })
 </script>
 @endpush
--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -3,12 +3,14 @@
 <div>
     <div class="container p-3 bg-white">
     <h3>Users</h3>
+    <div id="button"></div>
     <table id="user_table" class="table table-borderless display">
         <thead>
             <tr>
                 <th></th>
                 <th>Name</th>
                 <th>Username</th>
+                <th>Email</th>
                 <th>Register at</th>
                 <th>Level</th>
                 <th></th>
@@ -21,8 +23,9 @@
                 <td>{{$i++}}</td>
                 <td>{{$u->full_name}}</td>
                 <td>{{$u->username}}</td>
+                <td>{{$u->email}}</td>
                 <td>{{$u->created_at}}</td>
-                <td class="text-capitalize">{{$u->level}}</td>
+                <td>{{ucfirst($u->level)}}</td>
                 <td class="text-center"><a href="{{route('admin.user.detail', $u->id)}}" class="btn btn-sm btn-primary">View</a></td>
             </tr>
             @endforeach
@@ -33,13 +36,23 @@
 @endsection
 
 @push('css')
-    <link rel="stylesheet" href="{{asset('/vendor/datatable/datatable.css')}}">
+    <link rel="stylesheet" href="{{asset('/vendor/datatable/datatables.min.css')}}">
 @endpush
 @push('script')
-<script src="{{asset('vendor/datatable/datatable.js')}}"></script>
+<script src="{{asset('vendor/datatable/datatables.min.js')}}"></script>
 <script>
     $(document).ready(function(){
-        $('#user_table').DataTable();
+        var table = $('#user_table').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        columns: [0,1,2,3,4]
+                    }
+                }
+            ]
+        });
     })
 </script>
 @endpush
