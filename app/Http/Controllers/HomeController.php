7c789f9dea779963e96928f2608162f54fececba
zunkazaragi <malikal.j4@gmail.com>
2020-06-29 11:30:27 +0700

    Landing Page Recover

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,15 +15,6 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show landing page
